e6e66fbb56864575cb186e3beafe9c500895ee35
Marcel Juca <marceljuca@edu.unifor.br>
2025-08-01 17:49:24 -0300

    Organize Prefab structure
    - Created base Character prefab to split npcs and players.
    - Also created a prefab for world to avoid modifying scene

--- Assets/Scripts/Entity/EntityReferences.cs
+++ Assets/Scripts/Entity/EntityReferences.cs
@@ -4,7 +4,6 @@ namespace mpw.Entity
 {
     public class EntityReferences : MonoBehaviour
     {
-        [SerializeField] private Rigidbody rigidBody;
         [SerializeField] private Transform bodyTransform;
         [SerializeField] private CharacterController controller;
 
